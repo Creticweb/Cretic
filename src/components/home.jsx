9523cbf56b0a5ad302f734e44c1d2892f6e61771
IamShakal <gourabdas2927@gmail.com>
2020-11-07 22:14:22 +0530

    meta

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Service from "./service";
 import Join from "./join";
+import {Helmet} from "react-helmet";
 //import Banner from "./banner";
 import Process from "./process";
 import Footer from "./footer";
@@ -8,7 +9,10 @@ import Footer from "./footer";
 const Home = () => {
   return (
      <React.Fragment>
-     
+     <Helmet>
+            <title>Cretic Solutions offers a suite of online services</title>
+            <meta name="description" content="We offers a suite of online services like company registration, trademark filing, income tax filing, GST registration, GST return filing" />
+        </Helmet>
       <Service />
       <Process />
       <Join />
