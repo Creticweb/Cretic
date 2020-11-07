966b4bc25132f63c49d21cd5e2a4c573f33e6743
IamShakal <gourabdas2927@gmail.com>
2020-11-08 01:22:26 +0530

    remove

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Service from "./service";
 import Join from "./join";
 import {Helmet} from "react-helmet";
-import Banner from "./banner";
+//import Banner from "./banner";
 import Process from "./process";
 import Footer from "./footer";
 
@@ -13,11 +13,9 @@ const Home = () => {
             <title>Cretic Solutions offers a suite of online services</title>
             <meta name="description" content="We offers a suite of online services like company registration, trademark filing, income tax filing, GST registration, GST return filing" />
         </Helmet>
-        <Banner />
       <Service />
       <Process />
       <Join />
-      
       <Footer />
      </React.Fragment>
   );
